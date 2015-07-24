6507c15ac781ffd73a6bdf70e46c56674604d289
Benjamin Stegeman <benstegeman@festiva.cis.ksu.edu>
2015-07-23 21:42:07 -0500

    Potentially successful token retrival

--- server.js
+++ server.js
@@ -1,9 +1,11 @@
 var http = require('http'),
 	express = require('express'),
-	url = require('url');
+	url = require('url'),
+	request = require('request');
 
 const PORT=8080; 
 const CLIENT_ID = '40e3aa0f3dfd4cf9ba6c4034c0697f55';
+const CLIENT_SECRET = '371d5fdcbfbb4cca9b956547197230db';
 const GAME_URI = "http://localhost:8080/game";
 
 var app = express();
@@ -15,10 +17,27 @@ app.get('/', function (request, response) {
 	response.redirect('https://api.instagram.com/oauth/authorize/?client_id='+CLIENT_ID+'&redirect_uri=http://localhost:8080/game&response_type=code');
 });
 
-app.get('/game', function (request, response) {
-	if (request.hasOwnProperty('error')) {
-		response.send("GOOFED");
-		return;
-	}
-	response.send(request.code);
+app.get('/game', function (req, res) {
+	
+	//Get code to exchange for token
+	code = req.url.split('=')[1];
+
+	//POST request for token
+	
+	request.post(
+		'https://api.instagram.com/oauth/access_token',
+		{ form: { client_id:CLIENT_ID,
+			client_secret: CLIENT_SECRET,
+			grant_type: "authorization_code",
+			redirect_uri: GAME_URI,
+			code: code }
+		},
+		 function (err, resp, body) {
+		
+			console.log(body);
+		
+		 }
+	);
+	
+	res.end();
 });
