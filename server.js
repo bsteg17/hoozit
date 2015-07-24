050f61b5c88be2b0a28c1a14a6d572218f322ce0
bsteg17 <bstegeman17@gmail.com>
2015-07-23 20:20:17 -0500

    added token request

--- server.js
+++ server.js
@@ -1,9 +1,12 @@
 var http = require('http'),
 	express = require('express'),
-	url = require('url');
+	url = require('url'),
+	request = require('request');
 
 const PORT=8080; 
 const CLIENT_ID = '40e3aa0f3dfd4cf9ba6c4034c0697f55';
+const GRANT_TYPE = 'authorization_code';
+const CLIENT_SECRET = '371d5fdcbfbb4cca9b956547197230db';
 const GAME_URI = "http://localhost:8080/game";
 
 var app = express();
@@ -16,9 +19,39 @@ app.get('/', function (request, response) {
 });
 
 app.get('/game', function (request, response) {
-	if (request.hasOwnProperty('error')) {
-		response.send("GOOFED");
-		return;
-	}
-	response.send(request.code);
+
+	code = request.url.split('=')[1];
+
+    /*
+	path = //"/oauth/access_token?"
+			"client_id="+CLIENT_ID
+			+"&client_secret="+CLIENT_SECRET
+			+"&grant_type="+GRANT_TYPE
+			+"&redirect_url="+GAME_URI
+			+"&code="+code;
+
+	path = {client_id:CLIENT_ID,
+			client_secret:CLIENT_SECRET,
+			grant_type:GRANT_TYPE,
+			redirect_url:GAME_URI,
+			code:code};
+	*/
+
+	request({
+    	url: 'api.instagram.com/oauth/access_token', //URL to hit
+    	qs: {from: 'blog example', time: +new Date()}, //Query string data
+    	method: 'POST',
+    	//Lets post the following key/values as form
+    	form: {client_id:CLIENT_ID,
+			client_secret:CLIENT_SECRET,
+			grant_type:GRANT_TYPE,
+			redirect_url:GAME_URI,
+			code:code}
+	}, function(error, response, body){
+	    if(error) {
+	        console.log(error);
+	    } else {
+	        console.log(response.statusCode, body);
+	    }
+	});
 });
