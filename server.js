c595da84837236b5609b7b3000cf10b0b2f47228
bsteg17 <bstegeman17@gmail.com>
2015-10-25 11:22:43 -0500

    Retrieved user follow list JSON

--- server.js
+++ server.js
@@ -1,7 +1,8 @@
 var http = require('http'),
 	express = require('express'),
 	url = require('url'),
-	request = require('request');
+	request = require('request'),
+	curl = require('curlrequest');
 
 const PORT=8080; 
 const CLIENT_ID = '40e3aa0f3dfd4cf9ba6c4034c0697f55';
@@ -44,16 +45,36 @@ app.get('/game', function (req, res) {
 });
 
 var token_req = function (user_info) {
+
+	curl.request('https://api.instagram.com/v1/users/2245600125/follows?access_token='+user_info['access_token'], function (err, resp) {
+		console.log(resp);
+	});
+
+	/*
 	console.log(user_info);
 	console.log(user_info['user']['id']);
 	console.log(user_info['access_token']);
+	
 	options = {
 		host: 'www.instagram.com',
-		path: '/publicapi/v1/users/'+user_info['user']['id']+'/follows?access_token='+user_info['access_token']
+		path: '/v1/users/self/follows?access_token='+user_info['access_token'],
+		//path: 'v1/tags/nofilter/media/recent?client_id='+CLIENT_ID
+		encoding: null
 	};
-	http.request(options, function (resp) {
+
+	var full_data;
+	http.get(options, function (resp) {
+
+		//console.log(resp);
+
 		resp.on('data', function (data) {
-			console.log(data);
+			console.log(data.statusCode);
+			full_data += data.read();
 		});
-	}).end();
+		resp.on('end', function() {
+			var parsed = JSON.parse(full_data);
+			console.log(parsed);
+		});
+		
+	});*/
 }
